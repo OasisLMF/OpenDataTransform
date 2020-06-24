6dca6332bc68387cddbb7e6c522b793ef864317e
Dan Bate <dan.bate@gmail.com>
2020-06-24 16:44:19 +0100

    implemented CsvConnection

--- converter/connector/csv.py
+++ converter/connector/csv.py
@@ -1,11 +1,40 @@
+import csv
 from typing import Any, Dict, Iterable
 
 from converter.connector.base import BaseConnector
 
 
 class CsvConnector(BaseConnector):
+    def __init__(self, **options):
+        super().__init__(**options)
+
+        self.file_path = options["path"]
+        self.write_header = options.get("write_header", True)
+        self.quoting = {
+            "all": csv.QUOTE_ALL,
+            "minimal": csv.QUOTE_MINIMAL,
+            "none": csv.QUOTE_NONE,
+            "nonnumeric": csv.QUOTE_NONNUMERIC,
+        }.get(options.get("quoting", "nonnumeric"))
+
     def load(self, data: Iterable[Dict[str, Any]]):
-        pass
+        try:
+            data = iter(data)
+            first_row = next(data)
+        except StopIteration:
+            return
+
+        with open(self.file_path, "w", newline="") as f:
+            writer = csv.DictWriter(
+                f, fieldnames=first_row.keys(), quoting=self.quoting
+            )
+
+            if self.write_header:
+                writer.writeheader()
+
+            writer.writerow(first_row)
+            writer.writerows(data)
 
     def extract(self) -> Iterable[Dict[str, Any]]:
-        return []
+        with open(self.file_path, "r") as f:
+            yield from csv.DictReader(f, quoting=self.quoting)
---
+++ tests/connector/test_csv.py
@@ -0,0 +1,83 @@
+import csv
+import os
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+
+import pytest
+
+from converter.connector import CsvConnector
+
+
+def test_file_does_not_exist___error_is_raised():
+    connector = CsvConnector(path="somepath.csv")
+
+    with pytest.raises(FileNotFoundError):
+        list(connector.extract())
+
+
+def test_file_contains_data_with_heading___all_entries_are_loaded():
+    expected_data = [
+        {"a": 1, "b": 2},
+        {"a": 3, "b": 4},
+        {"a": 5, "b": 6},
+    ]
+
+    with NamedTemporaryFile("w+", newline="") as f:
+        writer = csv.DictWriter(
+            f, fieldnames=["a", "b"], quoting=csv.QUOTE_NONNUMERIC
+        )
+        writer.writeheader()
+        writer.writerows(expected_data)
+        f.flush()
+
+        assert list(CsvConnector(path=f.name).extract()) == expected_data
+
+
+def test_no_data_is_passed_to_loader___no_file_is_written():
+    with TemporaryDirectory() as p:
+        output_path = os.path.join(p, "result.csv")
+
+        CsvConnector(path=output_path).load([])
+
+        assert not os.path.exists(output_path)
+
+
+def test_data_is_passed_to_loader___data_with_heading_is_written_to_file():
+    expected_data = [
+        {"a": 1, "b": 2},
+        {"a": 3, "b": 4},
+        {"a": 5, "b": 6},
+    ]
+
+    with TemporaryDirectory() as p:
+        output_path = os.path.join(p, "result.csv")
+
+        CsvConnector(path=output_path).load(expected_data)
+
+        with open(output_path, "r") as f:
+            assert (
+                list(csv.DictReader(f, quoting=csv.QUOTE_NONNUMERIC))
+                == expected_data
+            )
+
+
+def test_data_is_passed_to_loader_no_header___data_is_written_to_file():
+    expected_data = [
+        {"a": 1, "b": 2},
+        {"a": 3, "b": 4},
+        {"a": 5, "b": 6},
+    ]
+
+    with TemporaryDirectory() as p:
+        output_path = os.path.join(p, "result.csv")
+
+        CsvConnector(path=output_path, write_header=False,).load(expected_data)
+
+        with open(output_path, "r") as f:
+            assert (
+                list(
+                    csv.DictReader(
+                        f, quoting=csv.QUOTE_NONNUMERIC, fieldnames=["a", "b"],
+                    )
+                )
+                == expected_data
+            )
