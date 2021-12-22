0402bb66495d4498369d5e1aee3894bd855d0d31
John Peterson <john@wildfish.com>
2021-12-22 17:04:20 +0000

    Update SQLite connector to read select and insert statements from a file rather than hard coded

--- converter/connector/errors.py
+++ converter/connector/errors.py
@@ -3,3 +3,17 @@ from converter.errors import ConverterError
 
 class SQLiteConnectionError(ConverterError):
     pass
+
+
+class SQLiteQueryError(ConverterError):
+    def __init__(self, query, data=None):
+        self.query = query
+        self.data = data
+
+        super().__init__(
+            f"Error running query: {query} with {data}."
+        )
+
+
+class SQLiteInsertDataError(ConverterError):
+    pass
