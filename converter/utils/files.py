5053067745c330367b2a5c2d13ae016d5748da4d
Dan Bate <dan@wildfish.com>
2023-07-28 15:13:44 +0100

    added adfs support

--- converter/utils/files.py
+++ converter/utils/files.py
@@ -9,13 +9,46 @@ def split_s3_url(parts):
     query = parse.parse_qs(parts.query)
     params = {
         "anon": "key" not in query and "token" not in query,
-        "key": query["key"][0],
-        "secret": query["secret"][0],
+        "key": query.get("key", [None])[0],
+        "secret": query.get("secret", [None])[0],
         "token": query.get("token", [None])[0],
         "use_ssl": query.get("ssl", ["True"])[0] == "True",
         "client_kwargs": {
             "endpoint_url": query.get("endpoint", [None])[0],
         },
+    }
+
+    return urllib.parse.urlunparse((
+        parts[0],
+        parts[1],
+        parts[2],
+        parts[3],
+        "",
+        parts[5],
+    )), params
+
+
+def split_azure_url(parts):
+    connection_string = None
+
+    query = parse.parse_qs(parts.query)
+    if "endpoint" in query:
+        connection_string = f"BlobEndpoint={ query.get('endpoint', [None])[0]};"
+    if "account" in query:
+        connection_string += f"AccountName={ query.get('account', [None])[0] };"
+    if "key" in query:
+        connection_string += f"AccountKey={ query.get('key', [None])[0] };"
+
+
+    params = {
+        "anon": "key" not in query and "secret" not in query,
+        "connection_string": connection_string,
+        "client_id": query.get("client", [None])[0],
+        "client_secret": query.get("secret", [None])[0],
+        "tenant_id": query.get("tenant", [None])[0],
+        "account_name": query.get("account", [None])[0],
+        "account_key": query.get("key", [None])[0],
+        "use_ssl": query.get("ssl", ["True"])[0] == "True",
     }
 
     return urllib.parse.urlunparse((
@@ -36,6 +69,8 @@ def parse_url_options(path):
     url_parts = parse.urlparse(path)
     if url_parts.scheme == "s3":
         path, params = split_s3_url(url_parts)
+    elif url_parts.scheme in ["az", "abfs", "adl"]:
+        path, params = split_azure_url(url_parts)
 
     return opener, path, params
 
